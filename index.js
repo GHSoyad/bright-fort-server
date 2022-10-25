3920648cc3639abdb859f14ba71e4860c8c55766
GHSoyad <ghsoyad@gmail.com>
2022-10-25 10:30:29 +0600

    Enabled cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
 const courses = require('./data/courses.json')
 
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Server is running')
